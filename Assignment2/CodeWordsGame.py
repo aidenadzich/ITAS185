50a9be91e7032ac772f3e4a630f56d7088d1da30
Aiden Adzich <aiden.adzich@itas.ca>
2024-11-21 12:05:24 -0800

    Updated Assignment2

--- Assignment2/CodeWordsGame.py
+++ Assignment2/CodeWordsGame.py
@@ -1,7 +1,7 @@
 import random
 import string
 import pywriter
-import colorama
+import colorama as c
 
 class CodeWordsGame:
     def __init__(self):
@@ -31,11 +31,11 @@ class CodeWordsGame:
         substitute = substitute.upper()
 
         if substitute in self.coded_word:
-            print(f"The letter '{substitute}' has already been used in the coded word. Choose a different substitute.")
+            pywriter.write(f"{c.Fore.RED}The letter {c.Fore.YELLOW}{substitute}{c.Fore.RED} has already been used in the coded word. Choose a different substitute.{c.Style.RESET_ALL}", rate = word_speed)
             return False
         
         if len(letter) > 1:
-            print("You can only select one letter!")
+            pywriter.write(f"{c.Fore.RED}You can only select one letter!{c.Style.RESET_ALL}", rate = word_speed)
             return False
 
         if letter.isalpha() and substitute.isalpha() and len(substitute) == 1:
@@ -43,10 +43,10 @@ class CodeWordsGame:
                 self.coded_word = self.coded_word.replace(letter, substitute)
                 return self.coded_word == self.secret_word
             else:
-                print(f"The letter '{letter}' is not part of the original secret word.")
+                pywriter.write(f"{c.Fore.RED}The letter {c.Fore.YELLOW}{letter}{c.Fore.RED} is not part of the original secret word.{c.Style.RESET_ALL}", rate = word_speed)
                 return False
         else:
-            print("Invalid input. Ensure you enter one valid letter for both fields.")
+            pywriter.write(f"{c.Fore.RED}Invalid input. Ensure you enter one valid letter for both fields.{c.Style.RESET_ALL}", rate = word_speed)
             return False
 
 
@@ -65,52 +65,71 @@ class CodeWordsGame:
         return self.coded_word == self.secret_word
 
     def is_lost(self):
-        return self.guesses_used > self.max_hints
+        return self.guesses_used >= self.max_guesses
 
     def __str__(self):
-        return f'Coded Word: {self.coded_word}'
+        return f'Coded Word: {c.Fore.MAGENTA}{self.coded_word}{c.Style.RESET_ALL}'
 
     def __repr__(self):
         return f'CodeWordsGame(secret_word={self.secret_word}, coded_word={self.coded_word}, max_hints={self.max_hints}, hints_used={self._hints_used})'
 
-    def __eq__(self, other):
-        return self.secret_word == other.secret_word
 
 if __name__ == "__main__":
     game = CodeWordsGame()
-    word_speed = 0.02
-    pywriter.write(f"Welcome to CodeWords. I have encoded a phrase as: {game.coded_word}", rate = word_speed)
-    pywriter.write(f"\nEnter a letter to select it for replacement", rate = word_speed)
-    pywriter.write(f"\nEnter '?' to use a hint. You have {game.max_hints} hints available to you", rate = word_speed)
-    pywriter.write(f"\nEnter '!' to make a guess. You have {game.max_guesses} guesses. If you run out, game over!", rate = word_speed)
-    print(game.secret_word)
+    word_speed = 0.03
+
+    pywriter.write(f"""{c.Fore.CYAN}
+*------------------------------------------------------*
+|   _____          _                            _      |
+|  / ____|        | |                          | |     |
+| | |     ___   __| | _____      _____  _ __ __| |___  |
+| | |    / _ \ / _` |/ _ \ \ /\ / / _ \| '__/ _` / __| |
+| | |___| (_) | (_| |  __/\ V  V / (_) | | | (_| \__ \ |
+|  \_____\___/ \__,_|\___| \_/\_/ \___/|_|  \__,_|___/ |
+|                  By: Aiden Adzich                    |
+*------------------------------------------------------*                                  
+     """, rate=0.01)
+
+    pywriter.write(f"\nWelcome to CodeWords. I have encoded a phrase as:{c.Fore.MAGENTA} {game.coded_word}", rate = word_speed)
+    pywriter.write(f"{c.Fore.GREEN}\nEnter a letter to select it for replacement", rate = word_speed)
+    pywriter.write(f"{c.Fore.BLUE}\nEnter {c.Fore.YELLOW}?{c.Fore.BLUE} to use a hint. You have {c.Fore.YELLOW}{game.max_hints}{c.Fore.BLUE} hints available to you", rate = word_speed)
+    pywriter.write(f"{c.Fore.BLUE}\nEnter {c.Fore.YELLOW}!{c.Fore.BLUE} to make a guess. You have {c.Fore.YELLOW}{game.max_guesses}{c.Fore.BLUE} guesses. If you run out, game over!", rate = word_speed)
+    pywriter.write(f"{c.Fore.RED}\nEnter {c.Fore.YELLOW}.{c.Fore.RED} to exit the program", rate = word_speed)
+    print(c.Style.RESET_ALL)
+    pywriter.write(f"\n{game}")
 
     while not game.is_won() and not game.is_lost():
         letter = input("\nMake your selection: ").strip()
         if letter == "?":
             hint = game.get_hint()
             if hint:
-                pywriter.write(f"\n{hint[0]} has been replaced with {hint[1]}. Now your phrase is:\n", rate = word_speed)
-                pywriter.write(game.coded_word, rate = word_speed)
-                pywriter.write(f"\nYou have {game.max_hints - game._hints_used} hints remaining", rate = word_speed)
+                pywriter.write(f"\n{c.Fore.YELLOW}{hint[0]}{c.Style.RESET_ALL} has been replaced with {c.Fore.YELLOW}{hint[1]}{c.Style.RESET_ALL}. Now your phrase is:\n", rate = word_speed)
+                pywriter.write(f"{c.Fore.MAGENTA} {game.coded_word} {c.Style.RESET_ALL}", rate = word_speed)
+                pywriter.write(f"\nYou have {c.Fore.YELLOW}{game.max_hints - game._hints_used}{c.Style.RESET_ALL} hints remaining", rate = word_speed)
             else:
-                pywriter.write("No hints remaining.", rate = word_speed)
+                pywriter.write(f"\n{c.Fore.RED}No hints remaining.{c.Style.RESET_ALL}", rate = word_speed)
 
         elif letter == "!":
-            guess = input("Make a guess for the game: ").upper()
-            #game.guess_word(guess)
+            guess = input("\nMake a guess for the game: ").upper()
             if guess == game.secret_word:
-                pywriter.write(f"\nCongrats! The word was {game.secret_word}!", rate = word_speed)
+                pywriter.write(f"{c.Fore.GREEN}\nCongrats! The word was {c.Fore.MAGENTA}{game.secret_word}{c.Style.RESET_ALL}", rate = word_speed)
                 exit()
             
             elif game.guesses_used >= game.max_guesses:
-                pywriter.write("Out of guesses :(", rate = word_speed)
+                pywriter.write(f"{c.Fore.RED}\nOut of guesses :({c.Style.RESET_ALL}", rate = word_speed)
                 exit()
 
             else:
-                pywriter.write(f"Incorrect. You have {game.max_guesses - game.guesses_used} guesses remaining", rate = word_speed)
                 game.guesses_used += 1
+                pywriter.write(f"{c.Fore.RED}Incorrect.{c.Style.RESET_ALL} You have {c.Fore.YELLOW}{game.max_guesses - game.guesses_used}{c.Style.RESET_ALL} guesses remaining", rate = word_speed)
+                
+
+        elif letter == ".":
+            pywriter.write("\nGoodbye!")
+            exit()
             
+        elif letter == "*":
+            print(game.__repr__())
 
         else:
             substitute = input(f"What letter do you want to replace '{letter}' with? ").strip()
@@ -118,12 +137,14 @@ if __name__ == "__main__":
                 pywriter.write(f"Congrats! The word was {game.secret_word}!", rate = word_speed)
                 break
             else:
-                print(game)
+                print(f"\ngame")
 
         if game.is_won():
-            pywriter.write(f"Congrats! The word was {game.secret_word}!", rate = word_speed)
+            pywriter.write(f"{c.Fore.GREEN}Congrats! The word was {c.Fore.MAGENTA}{game.secret_word}{c.Style.RESET_ALL}", rate = word_speed)
         
 
         if game.is_lost():
-            pywriter.write("You've used all your guesses. Game over!", rate = word_speed)
-            pywriter.write(f"The correct phrase was: {game.secret_word}", rate = word_speed)
+            pywriter.write(f"{c.Fore.RED}You've used all your guesses. Game over!", rate = word_speed)
+            pywriter.write(f"{c.Fore.GREEN}The correct phrase was: {c.Fore.MAGENTA}{game.secret_word}", rate = word_speed)
+
+print(c.Style.RESET_ALL)
